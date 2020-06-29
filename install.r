39b128f55d85460bb885a7d40cc1f9e2c65d993c
Naumasan <sandra.naumann@hu-berlin.de>
2020-06-29 16:18:36 +0200

    Upload_OSF

---
+++ install.r
@@ -0,0 +1,7 @@
+list.of.packages <- c("cowplot", "dplyr", "eeptools", "EnvStats", "ez", 
+                      "ggplot2", "ggstatsplot", "Hmisc", "lme4", "lmerTest", 
+                      "MASS", "miceadds", "psych", "reshape2", "Rmisc",
+                      "sjPlot", "sjmisc", "sjlabelled", "tidyverse") 
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
